ba39a2f42c6a7d3a96d90e0d865602df201b0a26
Rafaela Antunes Aguilhera <119620977+rafaAguilhera@users.noreply.github.com>
2023-05-08 15:28:45 -0300

    Create create-set-up.R

---
+++ analyze-cars/create-set-up.R
@@ -0,0 +1,24 @@
+library(e1071)
+library(ggplot2)
+
+
+# Library "e1071" is an R library that provides various functions for statistical and machine learning tasks, including classification, 
+# clustering, and probability distributions. It includes popular functions such as "svm()" for support vector machines, "naiveBayes()" 
+#for naive Bayes classifiers, and "kmeans()" for k-means clustering. It also includes functions for calculating various measures of 
+# association, such as mutual information and chi-squared statistics. The library is widely used in data science, machine learning, 
+# and predictive modeling.
+
+
+# The ggplot2 library is a data visualization library for the R programming language. It is an implementation of the Grammar of Graphics, 
+# which is a general scheme for creating statistical graphics. The library provides a high-level declarative syntax for creating a wide 
+# variety of plots, including scatter plots, line plots, bar plots, histograms, and more.
+
+# One of the key features of ggplot2 is its ability to easily overlay different types of data and visual elements. This allows users to
+# create complex and multi-layered plots that can convey a lot of information in a clear and concise manner.
+
+# In addition to its powerful plotting capabilities, ggplot2 also provides several tools for customizing the appearance of plots. 
+# This includes options for controlling the color, size, and shape of plotted elements, as well as the ability to add labels, 
+# annotations, and other text elements to plots.
+
+# ggplot2 is widely used in data science and data visualization and has a large and active community of users and developers. 
+# It is considered one of the most powerful and flexible data visualization libraries for R.
